11e81ad06dec89cde7b8fa95362777810da51dd5
V Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2025-05-14 17:39:50 +0530

    Update maximunandminimunarray.cpp

--- Maximum and minimum of an array/maximunandminimunarray.cpp
+++ Maximum and minimum of an array/maximunandminimunarray.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+  public:
+    pair<int, int> getMinMax(vector<int> arr) {
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for(int i = 0; i < arr.size(); i++) {
+            if (arr[i] < mini)
+            mini = arr[i];
+            if(arr[i] > maxi)
+            maxi = arr[i];
+        }
+        return {mini, maxi};
+    }
+};

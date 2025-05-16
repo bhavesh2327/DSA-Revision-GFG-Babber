108958992330445f134212fca48193f92514bd25
V Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2025-05-16 18:26:26 +0530

    Update kth-smallest-element.cpp

--- kth-smallest-element/kth-smallest-element.cpp
+++ kth-smallest-element/kth-smallest-element.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+  public: 
+int kthSmallest(vector<int>& arr, int k) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < arr.size(); i++) {
+        pq.push(arr[i]);
+    } 
+}
+};
